e5fb570998f40782ed161f48c4b71a503df7aab2
Chris Yin <ceyin.818@gmail.com>
2025-12-28 22:44:46 -0800

    removed mobile navigation hamburger menu to display nav inline

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -2,8 +2,6 @@ import headerNavLinks from '@/data/headerNavLinks';
 import Link from './Link';
 import SectionContainer from './SectionContainer';
 import Footer from './Footer';
-import MobileNav from './MobileNav';
-// import ThemeSwitch from './ThemeSwitch';
 
 const LayoutWrapper = ({ children }) => {
   return (
@@ -13,29 +11,23 @@ const LayoutWrapper = ({ children }) => {
         <div className="flex flex-col justify-between h-screen">
           <header className="flex items-center py-10">
             <div className="flex flex-auto items-center justify-between">
-              <div className="flex flex-1 items-center justify-between text-base leading-5">
-                <div className="hidden sm:block">
-                  {headerNavLinks.map((link) => (
-                    <Link
-                      key={link.title}
-                      href={link.href}
-                      className="p-2 mr-4 font-medium text-gray-900 sm:p-2 dark:text-gray-100  hover:underline hover:rounded "
-                    >
-                      {link.title}
-                    </Link>
-                  ))}
-                </div>
-              </div>
-
-              <MobileNav />
+              <nav className="flex flex-1 items-center text-base leading-5">
+                {headerNavLinks.map((link) => (
+                  <Link
+                    key={link.title}
+                    href={link.href}
+                    className="p-2 mr-4 font-medium text-gray-900 sm:p-2 dark:text-gray-100 hover:underline hover:rounded"
+                  >
+                    {link.title}
+                  </Link>
+                ))}
+              </nav>
             </div>
           </header>
           <main className="mb-auto">{children}</main>
-
           <Footer />
         </div>
       </SectionContainer>
-      {/* <FooterGradient /> */}
     </>
   );
 };
@@ -47,13 +39,5 @@ function HeaderGradient() {
     </div>
   );
 }
-
-// function FooterGradient() {
-//   return (
-//     <div className="mx-auto max-w-6xl motion-safe:animate-rotate-colors pointer-events-none z-[-1]">
-//       <div className="absolute inset-x-0 bg-gradient-to-r from-amber-500 via-indigo-500 to-emerald-500 rounded-t-full opacity-20 blur-3xl h-[200px]" />
-//     </div>
-//   );
-// }
 
 export default LayoutWrapper;
