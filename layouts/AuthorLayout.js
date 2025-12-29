25a005107b6920e5a82092b4fd892485c125ecb8
Chris Yin <ceyin.818@gmail.com>
2025-12-29 00:10:17 -0800

    changed page titles to peach color instead of white

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -3,17 +3,19 @@ import { PageSEO } from '@/components/SEO';
 
 export default function AuthorLayout({ children, frontMatter }) {
   const { name, avatar, occupation, company } = frontMatter;
-
   return (
     <>
       <PageSEO title={`About - ${name}`} description={`About me - ${name}`} />
       <div className="divide-y">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <h1
+            className="text-3xl font-extrabold leading-9 tracking-tight sm:text-4xl sm:leading-10 md:text-6xl md:leading-14"
+            style={{ color: '#fed7aa' }}
+          >
             About
           </h1>
         </div>
-        <div className="items-start space-y-2  xl:gap-x-8 xl:space-y-0">
+        <div className="items-start space-y-2 xl:gap-x-8 xl:space-y-0">
           {/* <div className="flex flex-col items-center pt-8 space-x-2">
             <Image
               src={avatar}
